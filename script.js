52164ef8ff64b9b29462105b31f13ba46be44605
poepoepipi <139007040+poepoepipi@users.noreply.github.com>
2026-08-05 21:13:22 +0200

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function sayHello() {
-    alert("Hello!");
+function openTab(tabName) {
+    let tabs = document.getElementsByClassName("tab");
+
+    for (let tab of tabs) {
+        tab.style.display = "none";
+    }
+
+    document.getElementById(tabName).style.display = "block";
 }
+
+openTab("home");
