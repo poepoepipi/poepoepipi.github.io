c894cf559c4f9ec000a155c1814f1ed195b9265b
poepoepipi <139007040+poepoepipi@users.noreply.github.com>
2026-08-06 17:03:44 +0200

    modified:   script.js

--- script.js
+++ script.js
@@ -10,10 +10,4 @@ function openTab(tabName) {
 
 openTab("home");
 
-fetch("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US")
-    .then(response => response.json())
-    .then(data => {
-        const imageUrl = "https://www.bing.com" + data.images[0].url;
-        document.body.style.backgroundImage = `url(${imageUrl})`;
-    })
-    .catch(error => console.error("Failed to load Bing wallpaper:", error));
+document.body.style.backgroundImage = "url(https://picsum.photos/1920/1080)";
